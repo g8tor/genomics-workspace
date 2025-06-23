79d94a3e6f20622c5e8fbd4c36edf03aa141552a
Vernon Chapman <vernon.chapman@usda.gov>
2025-06-23 17:57:28 -0400

    data: updated the script to generate api views

--- scripts/sql/generate-api.views.sql
+++ scripts/sql/generate-api.views.sql
@@ -16,7 +16,6 @@ CREATE OR REPLACE VIEW api.organisms AS
         split_part(po.display_name, ' ', 2) species,
         lower(po.short_name) as short_name,
         t.id as train_id
-
     FROM app_organism po
     LEFT JOIN training.app_organism t 
     ON po.display_name = t.display_name
@@ -31,8 +30,7 @@ CREATE OR REPLACE VIEW api.organism_updates AS
     AND o.id != o.train_id
     ORDER BY o,id ASC;
 
-
--- Create FAst File View
+-- Create Fasta File View
 CREATE OR REPLACE VIEW api.fastafiles AS
     SELECT id as fastafile_id, file_name, checksum
     FROM public.fastafiles 
@@ -41,27 +39,42 @@ CREATE OR REPLACE VIEW api.fastafiles AS
 -- Create Database View
 CREATE OR REPLACE VIEW api.databases AS
     SELECT f.fastafile_id as id, b.organism_id, b.type_id as sequencetype_id, f.fastafile_id, 
-           b.title, b.description, f.checksum
+           b.title, b.description, f.checksum, f.file_name
     FROM blast_blastdb b
     LEFT OUTER JOIN api.fastafiles f
     ON b.file_name = f.file_name
     ORDER BY title, checksum ASC;
 
--- Create Blast View
+-- Create Production BlastDBs View
+CREATE OR REPLACE VIEW api.production_blastdbs AS 
+    SELECT d.id as database_id, d.organism_id
+    FROM blast_blastdb b 
+    JOIN fastafiles f ON b.file_name = f.file_name
+    JOIN api.databases d ON d.checksum = f.checksum; 
 
--- SELECT id as database_id, organism_id, True as is_shown
--- FROM api.databases
--- ORDER BY database_id ASC
+-- Create Production HmmerDBs View
+CREATE OR REPLACE VIEW api.production_hmmerdbs AS 
+    SELECT d.id as database_id, d.organism_id
+    FROM hmmer_hmmerdb h 
+    LEFT JOIN fastafiles f ON h.file_name = f.file_name
+    LEFT JOIN api.databases d ON d.checksum = f.checksum; 
+
+-- Create Training BlastDBs View
+CREATE OR REPLACE VIEW api.training_blastdbs AS 
+    SELECT d.id as database_id, d.organism_id
+    FROM training.blast_blastdb b 
+    JOIN fastafiles f ON b.file_name = f.file_name
+    --JOIN api.databases d ON d.checksum = f.checksum
+    ; 
+
+-- Create Training HmmerDBs View
+CREATE OR REPLACE VIEW api.training_hmmerdbs AS 
+    SELECT d.id as database_id, d.organism_id
+    FROM training.hmmer_hmmerdb h 
+    LEFT JOIN fastafiles f ON h.file_name = f.file_name
+    LEFT JOIN api.databases d ON d.checksum = f.checksum; 
 
 
--- SELECT  h.file_name, f.fastafile_id, f.checksum
--- FROM hmmer_hmmerdb h
--- LEFT OUTER JOIN api.fastafiles f
--- ON h.file_name = f.file_name;
 
 
 
-SELECT d.database_id, d.organism_id
-FROM api.databases d
-RIGHT JOIN blast_blastdb b
-ON d.file_name = b.file_name;
