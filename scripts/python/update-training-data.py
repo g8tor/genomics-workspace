ddb0a8127d2323a8230891310b43ff9e87f5f75e
Vernon Chapman <vernon.chapman@usda.gov>
2025-06-25 23:05:54 -0400

    code: updated sql queries

--- scripts/python/update-training-data.py
+++ scripts/python/update-training-data.py
@@ -1,48 +1,46 @@
-from os import path ,environ, getcwd
+from os import path ,environ as env , getcwd
 import sys
 import pandas as pd
 import numpy as np
 import pdb, django
+from loguru import logger as log
+import pdb
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
-PATH_PREFIX = "/usr/local/i5k/media/blast/db"
+from pprint import pprint
+from django_pandas.io import read_frame
+from i5k.settings import DATABASES
+import psycopg2
+
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-environ['DJANGO_SETTINGS_MODULE'] = 'i5k.settings'
+env['DJANGO_SETTINGS_MODULE'] = 'i5k.settings'
 django.setup()
 
-from django_pandas.io import read_frame
-from blast.models import BlastDb, SequenceType, JbrowseSetting
-from hmmer.models import HmmerDB
-from app.models import Organism
+DB_USER = env.get('DB_USER')
+DB_PASS = env.get('DB_PASS')
+DB_HOST = env.get('DB_HOST')
+DB_PORT = env.get('DB_PORT')
+DB_HOST = env.get('DB_HOST')
 
-prefix =  "training"
-src_prefix = path.dirname(__file__)
+DATABASE_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/django'
 
-uo = pd.read_csv(path.join(src_prefix,f"{prefix}-organism-update.csv")).rename(
-    columns={'train_id':'organism','id':'organism_id'})
+pprint(DATABASE_URL)
 
-blast = pd.read_csv(path.join(src_prefix,f"{prefix}-blastdbs.csv"))
-bc = len(blast)
-bu =  blast.loc[blast.organism.isin(uo.organism.to_list())]
-blast.drop(bu.index,inplace=True)
-assert len(blast) + len(bu) == bc
-blast = pd.concat([blast,bu.merge(uo).drop(columns="organism").rename(
-    columns={"organism_id":"organism"})[blast.columns]])
-
-assert len(blast)  == bc
-
-
-hmmer = pd.read_csv(path.join(src_prefix,f"{prefix}-hmmerdbs.csv"))
-hc = len(hmmer)
-hu =  hmmer.loc[hmmer.organism.isin(uo.organism.to_list())]
-hmmer.drop(hu.index,inplace=True)
-assert len(hmmer) + len(hu) == hc
-hmmer = pd.concat([hmmer,hu.merge(uo).drop(columns="organism").rename(
-    columns={"organism_id":"organism"})[hmmer.columns]])
-
-assert len(hmmer) == hc
-
-
+def read_from_db(tbl:str, url:str=DATABASE_URL)->pd.DataFrame:
+    engine = create_engine(url, echo=False)
+    try:
+        
+        with engine.connect() as conn: #sessionmaker(autocommit=False, autoflush=False, bind=engine)() as sess:
+            query = f"SELECT * FROM {tbl};"
+            return pd.read_sql(query, conn)
+            
+    except Exception as exp:
+        raise exp
+    
+    
+dbs = read_from_db('api.databases')
 
 pdb.set_trace()
