bc1a8f66bc0ade3a7380cf0518ffe8be9c34c2eb
Vernon Chapman <vernon.chapman@usda.gov>
2025-06-27 22:08:40 -0400

    code: removed django / python src code

--- src/misc/logger.py
+++
@@ -1,8 +0,0 @@
-#
-#  Wrapper to return the i5k logger.
-#
-import logging
-
-def i5kLogger():
-    l = logging.getLogger('i5k')
-    return l
--- src/util/get_bin_name.py
+++
@@ -1,8 +0,0 @@
-from sys import platform
-
-
-def get_bin_name():
-    bin_name = 'bin_linux'
-    if platform == 'darwin':
-        bin_name = 'bin_mac'
-    return bin_name
