69cd728c85c58e29746c9ecd38d7a106e7eea22f
Vernon Chapman <g8tor692@gmail.com>
2025-07-13 12:52:01 -0400

    code: updated djangosql update script

--- files/scripts/sql/django-update.sql
+++ files/scripts/sql/django-update.sql
@@ -1,25 +1,18 @@
--- Create The Fasta File Table
--- \o /dev/null
-CREATE TABLE IF NOT EXISTS fastafiles (
-  id SERIAL,
-  checksum VARCHAR(200) NOT NULL,
-  file_name VARCHAR(300) NOT NULL,
-  is_shown  BOOLEAN NOT NULL DEFAULT True,
-  PRIMARY KEY (id),
-  UNIQUE(checksum),
-  UNIQUE(file_name)
-);
+ALTER DATABASE django SET client_min_messages TO ERROR;
+ALTER DATABASE django_training SET client_min_messages TO ERROR;
 
--- Make sure to copy the fastafiles.md5 to the root
--- directory if the postgresql server before running
--- this script from the django conrainer.
+\echo Begin Combining Django Dbs
 
--- Import the Fasta File Data
-COPY fastafiles(checksum, file_name)
-FROM '/fastafiles.md5'
-DELIMITER ',';
 
-\o
+-- -- Make sure to copy the fastafiles.md5 to the root
+-- -- directory if the postgresql server before running
+-- -- this script from the django conrainer.
+
+-- -- Import the Fasta File Data
+-- COPY fastafiles(checksum, file_name)
+-- FROM '/fastafiles.md5'
+-- DELIMITER ',';
+
 -- Create the extension that allows us to query across dbs
 CREATE EXTENSION postgres_fdw;
 
@@ -77,28 +70,52 @@ UPDATE training.blast_blastdb set fasta_file = trim(regexp_replace(fasta_file,'^
 UPDATE hmmer_hmmerdb set fasta_file = trim(regexp_replace(fasta_file,'^.*\/','')) where is_shown = True;
 UPDATE training.hmmer_hmmerdb set fasta_file = trim(regexp_replace(fasta_file,'^.*\/','')) where is_shown = True;
 -- Update the file_name column by removing the path
-UPDATE fastafiles SET file_name = trim(regexp_replace(file_name,'^.*\/',''));
+-- UPDATE fastafiles SET file_name = trim(regexp_replace(file_name,'^.*\/',''));
 
--- Drop THe Description Column
+-- Drop The Organism Description Column
 ALTER TABLE app_organism DROP COLUMN description;
 ALTER TABLE training.app_organism DROP COLUMN description;
 
 -- Rename the fasta_file columns to file_name
 ALTER TABLE blast_blastdb RENAME COLUMN fasta_file TO file_name;
+-- Remove the organism_id CONSTRAINT so that we can easily update the
+-- organism_id based on the combined api.organisms table
+ALTER TABLE blast_blastdb DROP CONSTRAINT blast_blastdb_organism_id_656c41fd_fk_app_organism_id;
+
+
 ALTER TABLE training.blast_blastdb RENAME COLUMN fasta_file TO file_name;
+\c django_training
+ALTER TABLE blast_blastdb DROP CONSTRAINT blast_blastdb_organism_id_656c41fd_fk_app_organism_id;
+\c django
 ALTER TABLE hmmer_hmmerdb RENAME COLUMN fasta_file TO file_name;
+ALTER TABLE hmmer_hmmerdb DROP CONSTRAINT hmmer_hmmerdb_organism_id_5d59d15b_fk_app_organism_id;
+
 ALTER TABLE training.hmmer_hmmerdb RENAME COLUMN fasta_file TO file_name;
-ALTER TABLE app_organism  ADD COLUMN train_id INTEGER UNIQUE;
+
+\c django_training
+ALTER TABLE hmmer_hmmerdb DROP CONSTRAINT hmmer_hmmerdb_organism_id_5d59d15b_fk_app_organism_id;
+
+\c django
+ALTER TABLE app_organism  ADD COLUMN organism_id INTEGER UNIQUE;
 
 -- Update the training ids for app organisms
 UPDATE
     app_organism ao
 SET
-    train_id = tao.id
+    organism_id = tao.id
 FROM
     training.app_organism tao
 WHERE
     ao.display_name = tao.display_name;
+
+UPDATE app_organism 
+SET organism_id = NULL
+WHERE id = organism_id;
+
+
+ALTER TABLE blast_blastdb ADD COLUMN new_organism_id INTEGER UNIQUE;
+
+
 
 UPDATE hmmer_hmmerdb
 SET title = regexp_replace(title,'^.*\/','')
@@ -111,24 +128,66 @@ UPDATE hmmer_hmmerdb
 SET title = regexp_replace(title,'^.*\/','')
 where title like '/%';
 
+\c django
 \o 
+\echo -------------------------------------------------------UPDATED DJANGO DATABASE--------------------------------------------------------------------
+\echo
 SELECT schemaname,relname, n_tup_ins - n_tup_del as rowcount 
 FROM pg_stat_all_tables 
 WHERE schemaname in ('public','training') AND relname NOT LIKE 'pg%' AND relname NOT LIKE 'sql%' order by schemaname, relname ASC;
+
+\c django_training
+SELECT schemaname,relname, n_tup_ins - n_tup_del as rowcount 
+FROM pg_stat_all_tables 
+WHERE schemaname in ('public','training') AND relname NOT LIKE 'pg%' AND relname NOT LIKE 'sql%' order by schemaname, relname ASC;
+
+
+\i /sql/combo.sql
 
 \c django
-SELECT schemaname,relname, n_tup_ins - n_tup_del as rowcount 
-FROM pg_stat_all_tables 
-WHERE schemaname in ('public','training') AND relname NOT LIKE 'pg%' AND relname NOT LIKE 'sql%' order by schemaname, relname ASC;
 
--- UPDATE
---     app_organism ao
--- SET
---     train_id = ao.id
--- WHERE
---     ao.train_id is NULL;
+\echo
+\echo ---------------------------------------------------------API DATABASE DATA-----------------------------------------------------------------------
+\echo
+SELECT schemaname,relname, n_tup_ins - n_tup_del as rowcount FROM pg_stat_all_tables WHERE relname NOT LIKE 'pg%' AND relname NOT LIKE
+'sql%' AND schemaname in ('api') order by schemaname, relname ASC ;
 
 
--- UPDATE hmmer_hmmerdb
--- SET title = regexp_replace(title,'^.*\/','')
--- where title like '/%';
+\c django_training
+
+\echo Drop API Database
+DROP DATABASE IF EXISTS api;
+\echo Recreate API Database 
+CREATE DATABASE api 
+WITH TEMPLATE django;
+ALTER DATABASE  api SET client_min_messages TO ERROR;
+\c api
+
+\echo Drop public schema
+
+do $$ declare
+    r record;
+begin
+    for r in (select tablename from pg_tables where schemaname = 'public') loop
+        execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+    end loop;
+end $$;
+
+DROP SCHEMA training CASCADE;
+
+\! rm api.pgc &> /dev/null
+\! pg_dump -n api  -U i5k -d api  -C -c --if-exists -Fc -f api.pgc 2>/dev/null
+
+\c api
+set search_path to api;
+ALTER TABLE collections SET SCHEMA public;
+ALTER TABLE fastafiles SET SCHEMA public;
+ALTER TABLE organisms SET SCHEMA public;
+ALTER TABLE sequencetypes SET SCHEMA public;
+-- CREATE SCHEMA public;
+-- CREATE SCHEMA production;
+-- CREATE SCHEMA training;
+\d
+\dn
+
+
