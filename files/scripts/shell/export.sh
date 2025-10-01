62111500490120ec8296fdb5687a3e279a9e6950
Vernon Chapman <g8tor692@gmail.com>
2025-10-01 06:40:41 -0400

    code: commented out unused code in anticipaion of being deleted

--- files/scripts/shell/export.sh
+++ files/scripts/shell/export.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
-export PGOPTIONS='--client-min-messages=warning'
-############################################
-# Set script variables for later use
-############################################
-OUTPUTDIR=/opt
-APIDB=${OUTPUTDIR}/api.db
+# export PGOPTIONS='--client-min-messages=warning'
+# ############################################
+# # Set script variables for later use
+# ############################################
+# OUTPUTDIR=/opt
+# APIDB=${OUTPUTDIR}/api.db
 
-SQLDIR=/sql
-UPDATE_SCRIPT=${SQLDIR}/django-update.sql
-GENERATE_SCRIPT=${SQLDIR}/generate-api-tables.sql
+# SQLDIR=/sql
+# UPDATE_SCRIPT=${SQLDIR}/django-update.sql
+# GENERATE_SCRIPT=${SQLDIR}/generate-api-tables.sql
 
-APIDB_SCHEMA=${SQLDIR}/api.schema
+# APIDB_SCHEMA=${SQLDIR}/api.schema
 
-if [ -d "${OUTPUTDIR}" ]; # Check that the $OUTPUTDIR exists
-then
-    # echo
-    # echo "Update apk database"
-    apk update &> /dev/null
+# if [ -d "${OUTPUTDIR}" ]; # Check that the $OUTPUTDIR exists
+# then
+#     # echo
+#     # echo "Update apk database"
+#     apk update &> /dev/null
 
-    # echo "Install nano and sqlite"
-    apk add nano sqlite &> /dev/null
+#     # echo "Install nano and sqlite"
+#     apk add nano sqlite &> /dev/null
 
-    # echo ""
+#     # echo ""
 
-    cd ${OUTPUTDIR} # Change into the $OUTPUTDIR
-    rm *.csv ${APIDB} &> /dev/null
+#     cd ${OUTPUTDIR} # Change into the $OUTPUTDIR
+#     rm *.csv ${APIDB} &> /dev/null
 
-    psql -U ${POSTGRES_USER} -d ${DB_NAME} <  ${UPDATE_SCRIPT} #&> /dev/null
-    if [ $? -eq 0 ]; # Check that the $UPDATE_SCRIPT ran successfully
-    then
-        echo "Updated Django DB"
-        psql -U ${POSTGRES_USER} -d ${DB_NAME} <  ${GENERATE_SCRIPT} &> /dev/null
-        if [ $? -eq 0 ]; # Check that the $GENERATE_SCRIPT ran successfully
-        then
-            echo "API Tables Successfully Exported"
-            sqlite3 ${APIDB} ".read ${APIDB_SCHEMA}"
-            if [ -f "${APIDB}" ]; # # Check that $APIDB was created
-            then
-                echo "API Database Created"
-                for fn in *.csv; do 
-                    TABLE=`basename -s .csv  ${fn}`
-                    sqlite3 api.db ".import --skip 1 --csv ${fn} ${TABLE}"
-                    if [ $? -eq 0 ];then
-                         count=`sqlite3 api.db " select count(*) from ${TABLE}"`
-                         echo "Imported ${count} records into ${TABLE}"
-                        # if [ -f "${fn}" ];then
-                        #     rm ${fn}
-                        # fi
-                    fi # Chweck that $fh was importted into $TABLE
-                done # Finish loop over *.csv files
-            fi # Finish checking for $APIDB
-        fi # Finish checking that $GENERATE_SCRIPT ran successfully
-    fi # Finish checking that $UPDATE_SCRIPT ran successfully
-    echo
-fi # Finish check that the $OUTPUTDIR exists
+#     psql -U ${POSTGRES_USER} -d ${DB_NAME} <  ${UPDATE_SCRIPT} #&> /dev/null
+#     if [ $? -eq 0 ]; # Check that the $UPDATE_SCRIPT ran successfully
+#     then
+#         echo "Updated Django DB"
+#         psql -U ${POSTGRES_USER} -d ${DB_NAME} <  ${GENERATE_SCRIPT} &> /dev/null
+#         if [ $? -eq 0 ]; # Check that the $GENERATE_SCRIPT ran successfully
+#         then
+#             echo "API Tables Successfully Exported"
+#             sqlite3 ${APIDB} ".read ${APIDB_SCHEMA}"
+#             if [ -f "${APIDB}" ]; # # Check that $APIDB was created
+#             then
+#                 echo "API Database Created"
+#                 for fn in *.csv; do 
+#                     TABLE=`basename -s .csv  ${fn}`
+#                     sqlite3 api.db ".import --skip 1 --csv ${fn} ${TABLE}"
+#                     if [ $? -eq 0 ];then
+#                          count=`sqlite3 api.db " select count(*) from ${TABLE}"`
+#                          echo "Imported ${count} records into ${TABLE}"
+#                         # if [ -f "${fn}" ];then
+#                         #     rm ${fn}
+#                         # fi
+#                     fi # Chweck that $fh was importted into $TABLE
+#                 done # Finish loop over *.csv files
+#             fi # Finish checking for $APIDB
+#         fi # Finish checking that $GENERATE_SCRIPT ran successfully
+#     fi # Finish checking that $UPDATE_SCRIPT ran successfully
+#     echo
+# fi # Finish check that the $OUTPUTDIR exists
